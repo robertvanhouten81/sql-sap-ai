ae5f4d7e2ba2a02660cbd355b2a114435f71ec7c
robertvanhouten81 <info@automatetheboringstuff.nl>
2025-01-16 16:37:19 +0100

    some prompt suggestion added

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
+
+"""
+
+    give me per employee how many orders he or she finished and the total amount of time (actual work) @bar
+
+    give me top 10 of the total actual costs with functional location and plant section @pie
+    
+    give me a percentage of the different order types @pie
+    
+    """
